106a8aef05ffcc10678ca8b8e833354cc2272f69
coffius <coffius@gmail.com>
2014-10-17 02:52:27 +0300

    проверка secret

--- src/main/scala/com/example/controllers/UserRestController.scala
+++ src/main/scala/com/example/controllers/UserRestController.scala
@@ -4,20 +4,29 @@ import spray.routing.Directives
 import spray.http.MediaTypes._
 import com.example.dto.{TokenResponse, TokenRequest}
 import spray.httpx.SprayJsonSupport._
-import spray.httpx.marshalling._
 import com.example.dto.JsonProtocol._
+import spray.http.StatusCode
 
 /**
  * @author coffius@gmail.com (Aleksei Shamenev)
  */
 class UserRestController extends Directives{
+  private val secretCode = "qwer"
   val route =
     path("token") {
       get {
         parameters('user_id.as[Int], 'secret.as[String]).as(TokenRequest){ tokenRequest: TokenRequest =>
-          respondWithMediaType(`application/json`) {
-            complete {
-              TokenResponse("sample_token")
+          if(tokenRequest.secret != secretCode){
+            respondWithStatus(StatusCode.int2StatusCode(404)){
+              complete{
+                ""
+              }
+            }
+          } else {
+            respondWithMediaType(`application/json`) {
+              complete {
+                TokenResponse("sample_token")
+              }
             }
           }
         }
