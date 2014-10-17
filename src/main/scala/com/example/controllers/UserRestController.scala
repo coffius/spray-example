c8d7363976365549ad0cb8eae885ed97e19f3678
coffius <coffius@gmail.com>
2014-10-17 16:41:57 +0300

    создание записи для юзера, если она была не найдена

--- src/main/scala/com/example/controllers/UserRestController.scala
+++ src/main/scala/com/example/controllers/UserRestController.scala
@@ -9,6 +9,9 @@ import spray.http.StatusCode
 import scala.slick.lifted.TableQuery
 import com.example.dao.Users
 import scala.slick.driver.PostgresDriver.simple._
+import com.example.model.User
+import java.util.UUID
+
 /**
  * @author coffius@gmail.com (Aleksei Shamenev)
  */
@@ -21,25 +24,30 @@ class UserRestController extends Directives with WithDb{
     path("token") {
       get {
         parameters('user_id.as[Int], 'secret.as[String]).as(TokenRequest){ tokenRequest: TokenRequest =>
+          //проверяем секретный ключ
           if(tokenRequest.secret != secretCode){
             respondWithStatus(StatusCode.int2StatusCode(404)){
               complete{ "" }
             }
           } else {
+            //если ключи совпадают - создаем новый или получаем существующий токен юзера
             db.withSession{ implicit session =>
               val userQuery = for{
                 u <- users if u.id === tokenRequest.userId
               } yield u
-              userQuery.firstOption.fold{
-                respondWithStatus(StatusCode.int2StatusCode(404)){ complete{ "" } }
-              }{ user =>
-                respondWithMediaType(`application/json`) {
-                  complete {
-                    TokenResponse(user.token)
-                  }
-                }
+              val userToken = userQuery.firstOption.fold{
+                val newToken: String = UUID.randomUUID().toString
+                users += User(id = Some(tokenRequest.userId), token = newToken)
+                newToken
+              }{
+                _.token
               }
 
+              respondWithMediaType(`application/json`) {
+                complete {
+                  TokenResponse(userToken)
+                }
+              }
             }
           }
         }
